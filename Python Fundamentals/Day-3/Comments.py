4c11802f88031762f5a329a777739e26139c1eb5
Samir Kumar Dash <samirkumardash264@gmail.com>
2026-07-26 19:31:18 +0530

    Day-3: Using Comments and Escape Sequences

---
+++ Python Fundamentals/Day-3/Comments.py
@@ -0,0 +1,11 @@
+'''Hey Samir, Please dont remove the line
+Author: Samir
+Course: Python Placement Prep'''
+print("Hey im a \"good boy\"\nand my friend is also a good boy/girl")
+
+print("Hello World")
+# This is a 'single-Line Comment'
+print("This is a print statement")
+print("Hello World") # Printing Hello World
+print("Hey", 6, 7, sep="~", end="009\n")
+print("Samir")
