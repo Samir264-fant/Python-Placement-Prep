28d04819934b9e28f3efc93d80f8a73083adbec5
Samir Kumar Dash <samirkumardash264@gmail.com>
2026-07-31 20:57:36 +0530

    Day-7: Storing movie names in a list

---
+++ Python Fundamentals/Day-7/movielist.py
@@ -0,0 +1,5 @@
+movie1 = input("Enter the first movie name : ")
+movie2 = input("Enter the second movie name : ")
+movie3 = input("Enter the third movie name : ")
+list = [movie1, movie2, movie3]
+print("The list of movies are:",list)
