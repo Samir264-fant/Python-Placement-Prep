8e9c67bbe3a60cfbe0d62dd566f8b8ebd990fb52
Samir Kumar Dash <samirkumardash264@gmail.com>
2026-08-02 23:02:37 +0530

    Day-8: Solving Dictionary Question

---
+++ Python Fundamentals/Day-8/marks.py
@@ -0,0 +1,7 @@
+Subject1 = input("Enter the subject 1 name:")
+Subject2 = input("Enter the subject 2 name:")
+Subject3 = input("Enter the subject 3 name:")
+dict = {
+    Subject1: "",Subject2: "",Subject3: ""  
+}
+print(dict)
