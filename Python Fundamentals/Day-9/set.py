b0878e8b00d87879ead281dc2d4cbff6d03bdf6e
Samir Kumar Dash <samirkumardash264@gmail.com>
2026-08-03 18:50:04 +0530

    Day-9: Operations in set

---
+++ Python Fundamentals/Day-9/set.py
@@ -0,0 +1,4 @@
+set1 = {"python", "java", "c++", "python", "javascript"}
+set2 = {"python", "java", "c++", "c", "java"}
+set3 = set1.union(set2)
+print("The classroom needed for students are", len(set3))
